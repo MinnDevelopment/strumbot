c893d5251a88acdf8d2232abe656e6947f4e5fea
Florian Spieß <business@minnced.club>
2019-12-12 16:20:17 +0100

    Improve error handling of rank service

--- src/main/kotlin/strumbot/main.kt
+++ src/main/kotlin/strumbot/main.kt
@@ -23,11 +23,20 @@ import club.minnced.jda.reactor.on
 import club.minnced.jda.reactor.then
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.Permission
+import net.dv8tion.jda.api.entities.Member
+import net.dv8tion.jda.api.entities.Message
+import net.dv8tion.jda.api.entities.MessageChannel
+import net.dv8tion.jda.api.entities.Role
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
+import net.dv8tion.jda.api.exceptions.HierarchyException
+import net.dv8tion.jda.api.exceptions.InsufficientPermissionException
+import net.dv8tion.jda.api.exceptions.PermissionException
 import net.dv8tion.jda.api.utils.cache.CacheFlag
 import okhttp3.OkHttpClient
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import reactor.core.publisher.Mono
 import reactor.core.scheduler.Schedulers
 import java.util.EnumSet.noneOf
 import java.util.concurrent.Executors
@@ -78,36 +87,69 @@ private fun setupRankListener(jda: JDA, configuration: Configuration) {
         .map { it.message }
         .filter { it.member != null }
         .filter { it.contentRaw.startsWith("?rank ") }
-        .flatMap {
-            val member = it.member!!
+        .flatMap { event ->
+            val member = event.member!!
             val mention = member.asMention
             // The role name is after the command
-            val roleName = it.contentRaw.removePrefix("?rank ").toLowerCase()
+            val roleName = event.contentRaw.removePrefix("?rank ").toLowerCase()
             // We shouldn't let users assign themselves any other roles like mod
+            val channel = event.channel
             if (roleName !in configuration.ranks) {
-                return@flatMap it.channel.sendMessage("$mention, That role does not exist!").asMono()
+                return@flatMap channel.sendMessage("$mention, That role does not exist!").asMono()
             }
 
             // Check if role by that name exists
-            val role = it.guild.getRolesByName(roleName, false).firstOrNull()
-            if (role != null) {
-                // Add/Remove the role to the member and send a success message
-                if (member.roles.any { it.idLong == role.idLong }) {
-                    log.debug("Adding ${role.name} to ${member.user.asTag}")
-                    it.guild.removeRoleFromMember(member, role).asMono().then {
-                        it.channel.sendMessage("$mention, you left **${role.name}**.").asMono()
-                    }
+            val role = event.guild.getRolesByName(roleName, false).firstOrNull()
+            Mono.defer {
+                if (role != null) {
+                    // Add/Remove the role to the member and send a success message
+                    toggleRole(member, role, event, channel, mention)
                 } else {
-                    log.debug("Removing ${role.name} from ${member.user.asTag}")
-                    it.guild.addRoleToMember(member, role).asMono().then {
-                        it.channel.sendMessage("$mention, you joined **${role.name}**.").asMono()
-                    }
+                    // Send a failure message, unknown role
+                    channel.sendMessage("$mention I don't know that role!").asMono()
                 }
-            } else {
-                // Send a failure message, unknown role
-                it.channel.sendMessage("$mention I don't know that role!").asMono()
+            }.doOnError(PermissionException::class.java) { error ->
+                handlePermissionError(error, channel, mention, role)
             }
         }
         .onErrorContinue { t, _ -> log.error("Rank service encountered exception", t) }
         .subscribe()
+}
+
+private fun toggleRole(
+    member: Member,
+    role: Role,
+    event: Message,
+    channel: MessageChannel,
+    mention: String
+): Mono<Message> {
+    return if (member.roles.any { it.idLong == role.idLong }) {
+        log.debug("Adding ${role.name} to ${member.user.asTag}")
+        event.guild.removeRoleFromMember(member, role).asMono().then {
+            channel.sendMessage("$mention, you left **${role.name}**.").asMono()
+        }
+    } else {
+        log.debug("Removing ${role.name} from ${member.user.asTag}")
+        event.guild.addRoleToMember(member, role).asMono().then {
+            channel.sendMessage("$mention, you joined **${role.name}**.").asMono()
+        }
+    }
+}
+
+private fun handlePermissionError(
+    error: PermissionException,
+    channel: MessageChannel,
+    mention: String,
+    role: Role?
+) {
+    if (error.permission == Permission.MESSAGE_WRITE || error.permission == Permission.MESSAGE_READ)
+        return // Don't attempt to send another message if it already failed because of it
+    when (error) {
+        is InsufficientPermissionException ->
+            channel.sendMessage("$mention, I'm missing the permission **${error.permission.getName()}**").queue()
+        is HierarchyException ->
+            channel.sendMessage("$mention, I can't assign a role to you because the role is too high! Role: ${role?.name}").queue()
+        else ->
+            channel.sendMessage("$mention, encountered an error: `$error`!").queue()
+    }
 }

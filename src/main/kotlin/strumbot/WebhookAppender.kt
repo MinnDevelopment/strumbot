51e3a75e53af7f90cf36a9ae2803c519f2c47bc4
Florian Spieß <business@minnced.club>
2022-01-08 02:09:14 +0100

    Use pattern encoder instead

--- src/main/kotlin/strumbot/WebhookAppender.kt
+++ src/main/kotlin/strumbot/WebhookAppender.kt
@@ -17,9 +17,9 @@
 package strumbot
 
 import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.classic.spi.LoggingEvent
 import ch.qos.logback.core.AppenderBase
-import ch.qos.logback.core.pattern.color.ANSIConstants
 import dev.minn.jda.ktx.Embed
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.entities.WebhookClient
@@ -34,68 +34,27 @@ class WebhookAppender : AppenderBase<LoggingEvent>() {
         }
     }
 
-    private object Color {
-        private fun csi(const: String, mod: String = "") = "${ANSIConstants.ESC_START}$mod$const${ANSIConstants.ESC_END}"
+    lateinit var encoder: PatternLayoutEncoder
 
-        val RESET = csi(ANSIConstants.DEFAULT_FG)
+    override fun start() {
+        if (!::encoder.isInitialized)
+            throw AssertionError("Missing pattern encoder")
 
-        val RED = csi(ANSIConstants.RED_FG)
-        val MAGENTA = csi(ANSIConstants.MAGENTA_FG)
-        val BLUE = csi(ANSIConstants.BLUE_FG)
-        val CYAN = csi(ANSIConstants.CYAN_FG)
-        val WHITE = csi(ANSIConstants.WHITE_FG)
-
-        val BOLD_RED = csi(ANSIConstants.RED_FG, ANSIConstants.BOLD)
-        val BOLD_MAGENTA = csi(ANSIConstants.MAGENTA_FG, ANSIConstants.BOLD)
-        val BOLD_BLUE = csi(ANSIConstants.BLUE_FG, ANSIConstants.BOLD)
-        val BOLD_CYAN = csi(ANSIConstants.CYAN_FG, ANSIConstants.BOLD)
-        val BOLD_WHITE = csi(ANSIConstants.WHITE_FG, ANSIConstants.BOLD)
+        encoder.start()
+        super.start()
     }
 
-    private fun highlight(level: Level) = when (level) {
-        Level.ERROR -> "${Color.BOLD_RED}${level.levelStr}${Color.RESET}"
-        Level.WARN -> "${Color.MAGENTA}${level.levelStr}${Color.RESET}"
-        Level.INFO -> "${Color.CYAN}${level.levelStr}${Color.RESET}"
-        Level.DEBUG -> "${Color.BLUE}${level.levelStr}${Color.RESET}"
-        Level.TRACE -> "${Color.WHITE}${level.levelStr}${Color.RESET}"
-        else -> level.levelStr
-    }
-
-    override fun append(eventObject: LoggingEvent) {
-        if (!eventObject.level.isGreaterOrEqual(Level.WARN)) return
+    override fun append(event: LoggingEvent) {
+        if (!event.level.isGreaterOrEqual(Level.WARN)) return
         val message = StringBuilder()
 
-        message.append("```ansi\n[")
-        message.append(highlight(eventObject.level))
-        message.append("] ")
-        message.append(eventObject.formattedMessage)
-
-        eventObject.throwableProxy?.let { throwable ->
-            message.append("\n")
-            message.append(Color.RED)
-            message.append(throwable.className)
-            message.append(": ")
-            message.append(throwable.message)
-            var done = true
-            for (elem in throwable.stackTraceElementProxyArray) {
-                val traceLine = elem.steAsString
-                if (traceLine.length + message.length >= 1500) {
-                    done = false
-                    break
-                }
-                message.append("\n\t")
-                message.append(traceLine)
-            }
-            if (!done)
-                message.append("\n\t...")
-            message.append(Color.RESET)
-        }
-
+        message.append("```ansi\n")
+        message.append(encoder.encode(event).toString(Charsets.UTF_8).take(1500))
         message.append("\n```")
 
         val embed = Embed(
             description = message.toString(),
-            timestamp = Instant.ofEpochMilli(eventObject.timeStamp)
+            timestamp = Instant.ofEpochMilli(event.timeStamp)
         )
         client?.sendMessageEmbeds(embed)?.queue(null) { it.printStackTrace() }
     }
